312ff8eb0c9bc663065f26c3d9e99dc20cb98aef
Tasnim Ferdous <tferdous611@gmail.com>
2023-10-31 13:04:57 -0400

    populate controller with the unique textfields and add btn from fxml

--- src/main/java/com/example/csc311_labweek09/HelloController.java
+++ src/main/java/com/example/csc311_labweek09/HelloController.java
@@ -1,14 +1,27 @@
 package com.example.csc311_labweek09;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    public TextField tfFirstName;
+    public TextField tfLastName;
+    public TextField tfEmail;
+    public TextField tfDOB;
+    public TextField tfZipCode;
+    public Button btnAdd;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void onBtnAddClick() {
+
     }
 }
