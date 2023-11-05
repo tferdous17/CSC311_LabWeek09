d0680485ec170f321771c2b81b5048431d6a9e25
Tasnim Ferdous <tferdous611@gmail.com>
2023-11-05 12:51:48 -0500

    add input validation for first name

--- src/main/java/com/example/csc311_labweek09/HelloController.java
+++ src/main/java/com/example/csc311_labweek09/HelloController.java
@@ -1,9 +1,15 @@
 package com.example.csc311_labweek09;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HelloController {
     public TextField tfFirstName;
@@ -12,16 +18,40 @@ public class HelloController {
     public TextField tfDOB;
     public TextField tfZipCode;
     public Button btnAdd;
+    public Label warningLabel;
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    boolean flag = false;
+
+    public void initialize() {
+        tfFirstName.setOnKeyPressed(event -> {
+            if (event.getCode() != KeyCode.TAB && flag){
+                tfFirstName.setStyle("-fx-border-color: #12c812 ; -fx-border-width: 1px ;");
+
+                flag = false;
+            }
+        });
+
+        tfFirstName.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                System.out.println("First name is focused");
+            } else {
+                if (tfFirstName.getText().matches("[a-zA-z]{2,25}")) {
+                    tfFirstName.setBorder(null);
+                    warningLabel.setText("");
+                } else {
+                    tfFirstName.setStyle("-fx-border-color: red ; -fx-border-width: 2px ;");
+                    tfFirstName.setVisible(true);
+                    tfFirstName.requestFocus();
+                    warningLabel.setText("WARNING: " + tfFirstName.getText() + " is not a valid first name.");
+                    flag = true;
+                }
+            }
+        });
+
     }
 
-    @FXML
-    public void onBtnAddClick() {
-
+    public void onBtnAddClick(ActionEvent actionEvent) {
     }
 }
